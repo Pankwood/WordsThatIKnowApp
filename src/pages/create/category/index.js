c5f9f4f1b46006a1e043139e0292ad81889bce44
Danilo Debiazi Vicente <dan.debiazi@gmail.com>
2020-08-05 15:40:58 -0400

    Simple example of useState

--- src/pages/create/category/index.js
+++ src/pages/create/category/index.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
 
 function CreateCategory(params) {
+  const [categoryName, setCategoryName] = useState("Filmes");
+  console.log("[categoryName]", categoryName);
+
   return (
     <PageDefault>
-      <h1>Create Category</h1>
+      <h1>Create Category {categoryName}</h1>
 
       <form>
         <label>
           Nome da Categoria:
-          <input type="text" />
+          <input
+            type="text"
+            value={categoryName}
+            onChange={function handleChange(params) {
+              setCategoryName(params.target.value);
+            }}
+          />
         </label>
 
         <button>Cadastrar</button>

10434f8506f0a5099781dde6dbb09342f7e4d962
Danilo Debiazi Vicente <dan.debiazi@gmail.com>
2020-08-05 12:34:51 -0400

    Added other components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import Menu from './components/Menu';
+import React from "react";
+import Menu from "./components/Menu";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+import data from "./data/data.json";
 
 function App() {
   return (
     <div>
-     <Menu />
+      <Menu />
+
+      <BannerMain
+        videoTitle={data.categories[0].videos[0].title}
+        url={data.categories[0].videos[0].url}
+        videoDescription={"O que é o Front-End?"}
+      />
+      <Carousel ignoreFirstVideo category={data.categories[0]}></Carousel>
+      <Carousel category={data.categories[1]}></Carousel>
+      <Carousel category={data.categories[2]}></Carousel>
+      <Carousel category={data.categories[3]}></Carousel>
+      <Footer />
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import Logo from '../../assets/img/Logo.gif';
-import './Menu.css';
-import Button from '../Button';
-
+import React from "react";
+import Logo from "../../assets/img/Logo.gif";
+import "./Menu.css";
+import Button from "../Button";
 
 function Menu(params) {
-    return(
-        <nav className='Menu'>
-            <a href='/'>
-<img className='Logo' src={Logo} alt='Logo Words That I Know' />
-
-            </a>
-            <Button as="a" href='/'>
-                Novo Video
-            </Button>
-        </nav>
-    )
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Logo Words That I Know" />
+      </a>
+      <Button as="a" className="ButtonLink" href="/">
+        Novo Video
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;

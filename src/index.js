887e7562ae9e33f8e0aa4db99cfec79053a4d18f
Danilo Debiazi Vicente <dan.debiazi@gmail.com>
2020-08-05 13:31:24 -0400

    Changed App to Home

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CreateVideo from "./pages/create/Video";
 import Page404 from "./pages/404";
+import Home from "./pages/Home/Home";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/create/video" component={CreateVideo} />
-      <Route path="/" component={App} exact />
+      <Route path="/" component={Home} exact />
       {/*<Route component={() => <div>Page 404</div>} />*/}
       <Route component={Page404} />
     </Switch>

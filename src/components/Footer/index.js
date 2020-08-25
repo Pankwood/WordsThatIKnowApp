0a6bd80c8ac886e1823f558a0419e190ca5585a7
dan <dan.debiazi@gmail.com>
2020-08-24 21:53:11 -0400

    Test Git Version

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import git from '../../webpack.config';
 
 function Footer() {
   return (
@@ -9,8 +10,8 @@ function Footer() {
         <a href="https://github.com/Pankwood/" target="_blank" rel="noopener noreferrer">
           Dan Debiazi
         </a>
-        - version 0.2.{process.env.VERSION}
       </p>
+      <p>version 0.2.{git.plugins.values}</p>
     </FooterBase>
   );
 }
---
+++ src/webpack.config.js
@@ -0,0 +1,23 @@
+/*const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const gitRevisionPlugin = new GitRevisionPlugin();
+
+module.exports = {
+    plugins: [
+        new GitRevisionPlugin({
+            'VERSION': JSON.stringify(gitRevisionPlugin.version()),
+            'COMMITHASH': JSON.stringify(gitRevisionPlugin.commithash()),
+            'BRANCH': JSON.stringify(gitRevisionPlugin.branch()),
+        }),
+    ]
+}*/
+
+
+
+var GitRevisionPlugin = require('git-revision-webpack-plugin')
+
+module.exports = {
+    plugins: [
+        new GitRevisionPlugin()
+    ]
+}
+

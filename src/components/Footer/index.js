067d0f3be0d5e53e88ab3a06371ce8f2c3b89d0f
dan <dan.debiazi@gmail.com>
2020-08-24 22:03:12 -0400

    Test Git Version3

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ function Footer() {
           Dan Debiazi
         </a>
       </p>
-      <p>version 0.3.{process.VERSIOM}</p>
+      <p>version 0.4.{process.env.VERSIOM}</p>
     </FooterBase>
   );
 }

4cd2e89f9e6705bebe3387d6f78f60b0448b2605
dan <dan.debiazi@gmail.com>
2020-09-17 19:57:52 -0400

    Improved Performance of Youtube iframe

--- src/components/BannerMain/components/VideoIframeResponsive/index.js
+++ src/components/BannerMain/components/VideoIframeResponsive/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Content from '../../../../data/Content/content.json';
+import { LocaleContext } from '../../../../LocaleContext';
 import { VideoContainer, ResponsiveIframe } from './styles';
 
 function YouTubeIframeResponsive({ youtubeID }) {
+  const [locale] = React.useContext(LocaleContext);
   return (
     <VideoContainer>
       <ResponsiveIframe
@@ -10,6 +12,8 @@ function YouTubeIframeResponsive({ youtubeID }) {
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${youtubeID}?autoplay=1&mute=1><img loading=lazy src=https://img.youtube.com/vi/${youtubeID}/hqdefault.jpg alt="${Content.language[locale].Home_title}"><span>&#x25BA;</span></a>`}
+        title={Content.language[locale].Home_title}
       />
     </VideoContainer>
   );
